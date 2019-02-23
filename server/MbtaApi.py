d943be670de5be06ca694a01baa4129fa6c7136d
Preston Mueller <skierjunk@gmail.com>
2019-02-22 19:01:11 -0500

    Forgot to urlencode the parameters

--- server/MbtaApi.py
+++ server/MbtaApi.py
@@ -7,5 +7,5 @@ BASE_URL_V3 = 'https://api-v3.mbta.com/{command}?{parameters}'
 def getV3(command, filter_name, filter_value, addl_params={}, api_key=secrets.API_KEY):
     headers = {'x-api-key': api_key}
     addl_params['filter[{}]'.format(filter_name)] = filter_value
-    response = requests.get(BASE_URL_V3.format(command=command, name=filter_name, value=filter_value, parameters=addl_params), headers=headers)
+    response = requests.get(BASE_URL_V3.format(command=command, name=filter_name, value=filter_value, parameters=urlencode(addl_params)), headers=headers)
     return response.json()
