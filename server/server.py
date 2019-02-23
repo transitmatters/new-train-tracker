de4bd2c77ec37f860e1da28d971d21abd6263fe4
Sky Rose <sky.q.rose@gmail.com>
2019-02-22 19:06:00 -0500

    server elm app from python server

--- server/server.py
+++ server/server.py
@@ -3,9 +3,17 @@ import MbtaApi
 
 app = flask.Flask(__name__)
 
-@app.route("/data")
-def data():
-    return 'Hello, transit world!'
+@app.route('/')
+def root():
+    return static_files("index.html")
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return flask.send_from_directory("../static", filename)
+
+@app.route('/js/<path:filename>')
+def js(filename):
+    return flask.send_from_directory("../js", filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
