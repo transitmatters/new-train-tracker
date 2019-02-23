39790d2190762f35f314eb528d7fa1bc3b41eded
Preston Mueller <skierjunk@gmail.com>
2019-02-22 19:06:11 -0500

    Remove manual fitering param completely, just take params

--- server/MbtaApi.py
+++ server/MbtaApi.py
@@ -4,8 +4,7 @@ from urllib.parse import urlencode
 
 BASE_URL_V3 = 'https://api-v3.mbta.com/{command}?{parameters}'
 
-def getV3(command, filter_name, filter_value, addl_params={}, api_key=secrets.API_KEY):
+def getV3(command, params={}, api_key=secrets.API_KEY):
     headers = {'x-api-key': api_key}
-    addl_params['filter[{}]'.format(filter_name)] = filter_value
-    response = requests.get(BASE_URL_V3.format(command=command, name=filter_name, value=filter_value, parameters=urlencode(addl_params)), headers=headers)
+    response = requests.get(BASE_URL_V3.format(command=command, parameters=urlencode(params)), headers=headers)
     return response.json()
