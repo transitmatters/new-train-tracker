60379f4d19363444bc9eed004d717d91b98c01c7
Preston Mueller <skierjunk@gmail.com>
2019-02-22 20:31:50 -0500

    Inline MbtaApi vehicle transformation function and add some doc comments

--- server/MbtaApi.py
+++ server/MbtaApi.py
@@ -1,29 +1,29 @@
-import secrets
+from urllib.parse import urlencode
 import requests
 import json_api_doc
+import secrets
 import Fleet
-from urllib.parse import urlencode
 
 BASE_URL_V3 = 'https://api-v3.mbta.com/{command}?{parameters}'
 
 def getV3(command, params={}, api_key=secrets.API_KEY):
+    """Make a GET request against the MBTA v3 API"""
     headers = {'x-api-key': api_key}
     response = requests.get(BASE_URL_V3.format(command=command, parameters=urlencode(params)), headers=headers)
     return json_api_doc.parse(response.json())
 
-def transform_vehicle(vehicle):
-    return {
+def vehicle_data_for_route(route):
+    """Use getv3 to request real-time vehicle data for a given route string"""
+    vehicles = getV3('vehicles', {
+        'filter[route]': route,
+        'include': 'stop'
+    })
+
+    return list(map(lambda vehicle: {
         'label': vehicle['label'],
         'route': vehicle['route']['id'],
         'current_status': vehicle['current_status'],
         'station_id': vehicle['stop']['parent_station']['id'],
         'new_flag': Fleet.car_array_is_new(vehicle['route']['id'],
                     vehicle['label'].split('-'))
-    }
-
-def vehicle_data_for_route(route):
-    vehicles = getV3('vehicles', {
-        'filter[route]': route,
-        'include': 'stop'
-    })
-    return list(map(transform_vehicle, vehicles))
+    }, vehicles))
