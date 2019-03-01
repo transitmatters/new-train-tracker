f7e52d167185c2f8dfd549a05f2ed2810bfb2438
Preston Mueller <skierjunk@gmail.com>
2019-02-28 19:25:52 -0500

    Change Flask data to support multiple routes

--- server/MbtaApi.py
+++ server/MbtaApi.py
@@ -12,10 +12,10 @@ def getV3(command, params={}, api_key=secrets.API_KEY):
     response = requests.get(BASE_URL_V3.format(command=command, parameters=urlencode(params)), headers=headers)
     return json_api_doc.parse(response.json())
 
-def vehicle_data_for_route(route):
-    """Use getv3 to request real-time vehicle data for a given route string"""
+def vehicle_data_for_routes(routes):
+    """Use getv3 to request real-time vehicle data for a given route set"""
     vehicles = getV3('vehicles', {
-        'filter[route]': route,
+        'filter[route]': ','.join(routes),
         'include': 'stop'
     })
 
