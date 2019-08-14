00be9977d2192f0ffa0355c93bfa8ca57ed11f40
Preston Mueller <skierjunk@gmail.com>
2019-08-13 21:47:55 -0400

    Change server.py to application.py

--- server/application.py
+++ server/application.py
@@ -2,33 +2,33 @@ import flask
 import MbtaApi
 import json
 
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def root():
     return static_files("index.html")
 
-@app.route('/<path:filename>')
+@application.route('/<path:filename>')
 def static_files(filename):
     return flask.send_from_directory("../static", filename)
 
-@app.route('/assets/<path:filename>')
+@application.route('/assets/<path:filename>')
 def assets(filename):
     return flask.send_from_directory("../assets", filename)
 
-@app.route('/js/<path:filename>')
+@application.route('/js/<path:filename>')
 def js(filename):
     return flask.send_from_directory("../js", filename)
 
 # Data routes
-@app.route("/data/<routes>")
+@application.route("/data/<routes>")
 def data(routes):
     return flask.Response(json.dumps(MbtaApi.vehicle_data_for_routes(routes.split(','))))
 
-@app.route("/stops/<route>")
+@application.route("/stops/<route>")
 def stops(route):
     return flask.Response(json.dumps(MbtaApi.stops_for_route(route)))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
