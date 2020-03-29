02f8384368bb60013bc29745a8aa321f662efc1c
Ian Reynolds <idreyn@gmail.com>
2020-03-29 12:33:33 -0400

    going to try to add a carousel lol

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,21 @@ import LinePane from './LinePane';
 const App = () => {
     const api = useMbtaApi([greenLine]);
 
-    const renderInner = () => {
+    const renderMain = () => {
         if (api.isReady) {
             return <LinePane active={true} line={greenLine} api={api} />;
         }
     };
 
-    return <div className="app">{renderInner()}</div>;
+    return (
+        <div className="app">
+            <div className="header">
+                <h1>MBTA New Train Tracker</h1>
+                <div className="subtitle">something something TransitMatters</div>
+            </div>
+            {renderMain()}
+        </div>
+    );
 };
 
 export default App;
