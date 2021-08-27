75d076858e1c4b22dcae09fefce2c7ca29e29f79
Nathan Weinberg <nathan2@stwmd.net>
2021-08-27 11:34:48 -0400

    Lint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,12 @@ const Footer = React.forwardRef((props, ref) => {
     return (
         <div className="footer" ref={ref}>
             <p>
-                <a href="https://transitmatters.org/transitmatters-labs">TransitMatters Labs</a> | <a href="https://github.com/transitmatters/new-train-tracker">Source code</a> | <a href="mailto:labs@transitmatters.org?subject=[Train%20Tracker%20Feedback]%20-%20">🚀 Send feedback</a> | version {version}
+                <a href="https://transitmatters.org/transitmatters-labs">TransitMatters Labs</a> |{' '}
+                <a href="https://github.com/transitmatters/new-train-tracker">Source code</a> |{' '}
+                <a href="mailto:labs@transitmatters.org?subject=[Train%20Tracker%20Feedback]%20-%20">
+                    🚀 Send feedback
+                </a>{' '}
+                | version {version}
             </p>
         </div>
     );
