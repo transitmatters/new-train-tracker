689565f4e922b6c913f5dccefb9646b5d147ea4c
Nathan Weinberg <nathan2@stwmd.net>
2021-12-06 21:50:09 -0600

    Added static data to inital request payload

--- server/mbta_api.py
+++ server/mbta_api.py
@@ -217,11 +217,17 @@ async def routes_info(route_ids):
     return routes_to_return
 
 
+def get_static_data():
+    with open('../static/static_data.json') as f:
+        static_data = json.load(f)
+    return static_data
+
+
 def get_git_tag():
     return str(subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]))[2:-3]
 
 
-# captures intitial request data from MBTA API as well as server-side data such as git tags
+# captures initial request data from MBTA API as well as server-side data such as git tags and static train data
 # returns JSON of all data
 async def initial_request_data(route_ids, test_mode=False):
     routes, vehicle_data, *stops = await asyncio.gather(
@@ -233,6 +239,7 @@ async def initial_request_data(route_ids, test_mode=False):
     )
     sightings = get_recent_sightings_for_lines()
     git_tag = get_git_tag()
+    static_data = get_static_data()
     return {
         "version": git_tag,
         "sightings": sightings,
@@ -240,4 +247,5 @@ async def initial_request_data(route_ids, test_mode=False):
         "vehicles": vehicle_data[0],
         "vehicle_stats": vehicle_data[1],
         "stops": dict(zip(route_ids, stops)),
+        "static_data": static_data
     }
