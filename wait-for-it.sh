6eec4086de724f39ed93b425f8d9199abbd41bbd
Joshua Decosta <josh@Joshuas-M1.lan>
2022-07-02 17:16:27 -0400

    adds script for building

---
+++ wait-for-it.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+until ping -c 1 postgres >/dev/null 2>&1; do echo "not up"; done
+
+if [ "$NEW_BUILD" = true ]; then
+    npm run create-history-db 
+else
+    echo "old build"
+fi 
+
+/usr/local/bin/pipenv run gunicorn -b localhost:5001 -w 1 server.application:application
