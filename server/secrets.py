8dfa2cb7e023314a6b1762892611d3a30e439283
Preston Mueller <skierjunk@gmail.com>
2022-08-03 20:48:35 -0400

    Containerize NTT (#90)
    Co-authored-by: Joshua Decosta <joshdecosta@gmail.com>
    Co-authored-by: Thiago Teixeira <thiteixeira@gmail.com>

--- server/secrets.py
+++ server/secrets.py
@@ -1,8 +1,11 @@
 MBTA_V3_API_KEY = ''
 POSTGRES_ENABLED = True
 POSTGRES_DB = 'newtrains'
-POSTGRES_USER = 'ubuntu'  # Update to your Postgres user account if not using ubuntu
-POSTGRES_PASS = ''  # Add password if required; Uses account auth on linux
+# POSTGRES_HOST = 'postgres'
+
+# current set to root as the postgres db on docker doesn't by default create a separate user
+POSTGRES_USER = 'root'  # Update to your Postgres user account if not using ubuntu
+POSTGRES_PASS = 'password'  # Add password if required; Uses account auth on linux
 '''
 If you put your api key here, you may want to run
 `git update-index --assume-unchanged server/secrets.py`
