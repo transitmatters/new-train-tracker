9ade4fde9ed4370077c77a411e16be1cac4c9e17
Devin Matte <devinmatte@gmail.com>
2025-03-29 18:17:40 -0400

    Improve delivery stats for all lines, update to March (#237)

--- src/components/LineStats/LineStats.tsx
+++ src/components/LineStats/LineStats.tsx
@@ -1,5 +1,7 @@
 import * as stats from '../../../public/static_data.json';
 import { LineName } from '../../types';
+import { LastUpdatedStats } from './LastUpdatedStats';
+import { LineStatsTable } from './LineStatsTable';
 import './LineStats.css';
 
 interface Props {
@@ -7,68 +9,13 @@ interface Props {
 }
 
 export const LineStats: React.FunctionComponent<Props> = ({ line }) => {
-    const lineStats: {
-        totalNewDelivered?: number;
-        totalNewUndelivered?: number;
-        totalOldActive?: number;
-        totalOldInactive?: number;
-        totalActive?: number;
-        totalInactive?: number;
-    } = stats[line];
-
-    if (line === 'Blue') {
-        return (
-            <details className="stats-container">
-                <summary className="stats-title">Stats for {line} line</summary>
-                {lineStats ? (
-                    <table className="stats-table">
-                        <tbody>
-                            <tr>
-                                <td>Train Cars Active:</td>
-                                <td>{lineStats?.totalOldActive}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                ) : null}
-                <div className={'updated'}>
-                    <span style={{ fontWeight: 'bold' }}>Delivery info last updated: </span>
-                    <span>{new Date(stats.Updated).toDateString()}</span> (
-                    <a href={stats.Sources.fleet_numbers}>source</a>)
-                </div>
-            </details>
-        );
-    }
+    const lineStats = stats[line];
 
     return (
         <details className="stats-container">
             <summary className="stats-title">Stats for {line} line</summary>
-            {lineStats ? (
-                <table className="stats-table">
-                    <tbody>
-                        <tr>
-                            <td>New Train Cars Delivered:</td>
-                            <td>{lineStats?.totalNewDelivered}</td>
-                        </tr>
-                        <tr>
-                            <td>New Train Cars Awaiting Delivery:</td>
-                            <td>{lineStats?.totalNewUndelivered}</td>
-                        </tr>
-                        <tr>
-                            <td>Old Train Cars Active:</td>
-                            <td>{lineStats?.totalOldActive}</td>
-                        </tr>
-                        <tr>
-                            <td>Old Train Cars Inactive:</td>
-                            <td>{lineStats?.totalOldInactive}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            ) : null}
-            <div className={'updated'}>
-                <span style={{ fontWeight: 'bold' }}>Delivery info last updated: </span>
-                <span>{new Date(stats.Updated).toDateString()}</span> (
-                <a href={stats.Sources.fleet_numbers}>source</a>)
-            </div>
+            {lineStats && <LineStatsTable line={line} stats={lineStats} />}
+            <LastUpdatedStats stats={stats} />
         </details>
     );
 };
