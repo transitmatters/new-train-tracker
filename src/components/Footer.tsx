ee7bde7ad6fabc99e4f6a2e2139bf259b2ea021f
Nathan Weinberg <nathansweinberg@gmail.com>
2025-04-28 20:06:33 -0400

    Add donate link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,11 @@ export const Footer: React.FC<{ version?: string }> = ({ version }) => {
     return (
         <div className="footer">
             <p>
-                <a href="https://transitmatters.org/transitmatters-labs">TransitMatters Labs</a> |{' '}
-                <a href="https://github.com/transitmatters/new-train-tracker">Source code</a> |{' '}
+                <a href="https://transitmatters.org/transitmatters-labs">About</a> |{' '}
+                <a href="https://github.com/transitmatters/new-train-tracker">Source Code</a> |{' '}
+                <a href="https://transitmatters.org/donate">Donate</a> |{' '}
                 <a href="mailto:labs@transitmatters.org?subject=[Train%20Tracker%20Feedback]%20-%20">
-                    Send feedback
+                    Feedback
                 </a>{' '}
                 | {version ? `v${version}` : 'dev'}
             </p>
