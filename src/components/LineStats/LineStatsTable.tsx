de8b4d9fd22df1582659cb43720c3ff79bf97203
Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
2025-12-23 13:21:04 -0600

    chore: update static data for cars (#269)
    * chore: update static data for cars
    
    also rename GL Type 9 vars from "Delivered/Undelivered"
    to "Active/Inactive" as Type 9 deliveries ended a while
    back and no more Type 9 cars will be ordered
    
    * chore: update CODEOWNERS

--- src/components/LineStats/LineStatsTable.tsx
+++ src/components/LineStats/LineStatsTable.tsx
@@ -9,8 +9,8 @@ interface LineStatsTableProps {
         totalType7Inactive?: number;
         totalType8Active?: number;
         totalType8Inactive?: number;
-        totalType9Delivered?: number;
-        totalType9Undelivered?: number;
+        totalType9Active?: number;
+        totalType9Inactive?: number;
         totalType10Delivered?: number;
         totalType10Undelivered?: number;
         totalOldActive?: number;
@@ -42,17 +42,15 @@ export const LineStatsTable: React.FC<LineStatsTableProps> = ({ line, stats }) =
                     </tr>
                     <tr>
                         <td>
-                            New <span className="train-type-text">Type 9</span> Train Cars
-                            Delivered:
+                            New <span className="train-type-text">Type 9</span> Train Cars Active:
                         </td>
-                        <td className="stat-count">{stats.totalType9Delivered}</td>
+                        <td className="stat-count">{stats.totalType9Active}</td>
                     </tr>
                     <tr>
                         <td>
-                            New <span className="train-type-text">Type 9</span> Train Cars Awaiting
-                            Delivery:
+                            New <span className="train-type-text">Type 9</span> Train Cars Inactive:
                         </td>
-                        <td className="stat-count">{stats.totalType9Undelivered}</td>
+                        <td className="stat-count">{stats.totalType9Inactive}</td>
                     </tr>
                     <br />
                     <tr>
